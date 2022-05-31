7fdea199657a761f1a7b251b68065fb0ab843b31
Wesley Santos <wesleysantosdev@outlook.com>
2022-05-31 19:38:02 -0300

    project finished

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-const number = document.querySelector('.bs-list')
-const list = document.getElementsByTagName('li')
+const list = document.querySelectorAll('.bs-list-number')
+const rating = document.querySelector('.selected-rating')
 
-number.addEventListener('click', () => {
-    // list[].style.background = 'red'
-})
+
+list.forEach((item, i) => {
+    item.addEventListener('click', () => {
+        list.forEach((otherItem, otherIndex) => {
+            item.classList.add('active')
+            if (otherIndex > i || otherIndex < i){
+                item.classList.add('active')
+                otherItem.classList.remove('active')
+            }
+        })
+        const index = parseInt(item.getAttribute('data-rate'))
+        localStorage.setItem('indexItem', index);
+    })
+});
+
+rating.textContent = localStorage.getItem('indexItem')
